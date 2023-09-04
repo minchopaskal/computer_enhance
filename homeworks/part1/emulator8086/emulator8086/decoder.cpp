e00c769901d85588f7b7fa76da8a53d6c8e44f47
Mincho Paskalev <minchopaskal@gmail.com>
2023-09-04 21:53:44 +0300

    listing_0039

--- homeworks/part1/emulator8086/emulator8086/decoder.cpp
+++ homeworks/part1/emulator8086/emulator8086/decoder.cpp
@@ -1,6 +1,8 @@
 #include "decoder.h"
 #include "instructions.h"
 
+#include <bit>
+#include <cassert>
 #include <cstdio>
 
 namespace emu8086 {
@@ -19,16 +21,32 @@ enum DataMask {
 };
 
 enum MemoryMode {
-	MEMORY,
+	NO_DISPLACEMENT,
 	SHORT,
 	WIDE,
 	REGISTER,
 };
 
 const char *reg_table[2][8] = {
-	{ "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh" },
-	{ "ax", "cx", "dx", "bx", "sp", "bp", "si", "di" }
+	{ "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh", },
+	{ "ax", "cx", "dx", "bx", "sp", "bp", "si", "di", },
 };
+
+const char *regmem_table[8] = {
+	"bx + si",
+	"bx + di",
+	"bp + si",
+	"bp + di",
+	"si",
+	"di",
+	"bp",
+	"bx",
+};
+
+bool check_opcode(uint8_t instr, Instruction opcode) {
+	int offset = std::countl_zero(uint8_t(opcode));
+	return ((instr >> offset) & opcode) == opcode;
+}
 
 void decode(const uint8_t *source, std::size_t source_size) {
 	// Incdicate asm is  16 bits arch
@@ -36,43 +54,93 @@ void decode(const uint8_t *source, std::size_t source_size) {
 	
 	std::size_t ptr = 0;
 	while (ptr < source_size) {
-		const uint8_t instr = source[ptr];
-		const uint8_t mem = source[ptr + 1];
+		const uint8_t instr = source[ptr++];
+		//fprintf(stdout, "INSTRUCTION: %x ", instr);
 
-		const uint8_t opcode = (instr & InstructionMask::OPCODE) >> 2;
-		const bool dest = (instr & InstructionMask::D) >> 1;
-		const bool wide = (instr & InstructionMask::W);
+		if (check_opcode(instr, Instruction::IMM_TO_REG)) {
+			const bool wide = (instr & 0x8);
+			const bool reg = (instr & 0x7);
+			
+			uint8_t data_l = source[ptr++];
+			uint8_t data_h = wide ? source[ptr++] : 0;
+			uint16_t data = data_l | (data_h << 8);
 
-		const uint8_t mode = (mem & DataMask::MOD) >> 6;
-		const uint8_t reg = (mem & DataMask::REG) >> 3;
-		const uint8_t regmem = (mem & DataMask::REGMEM);
+			const char *r = reg_table[wide][reg];
+			fprintf(stdout, "mov %s, %d\n", r, data);
 
-		//printf("dest: %d Wide: %d mode: %d reg: %d regmem: %d\n", dest, wide, mode, reg, regmem);
+			continue;
+		}
 
-		switch (opcode) {
-		case Instruction::MOV: {
-			switch (mode) {
-			case MemoryMode::REGISTER: {
+		if (check_opcode(instr, Instruction::REGMEM_TOFROM_REG)) {
+			const uint8_t mem = source[ptr++];
+
+			const bool dest = (instr & InstructionMask::D) >> 1;
+			const bool wide = (instr & InstructionMask::W);
+
+			const uint8_t mode = (mem & DataMask::MOD) >> 6;
+			const uint8_t reg = (mem & DataMask::REG) >> 3;
+			const uint8_t regmem = (mem & DataMask::REGMEM);
+
+			if (mode == MemoryMode::REGISTER) {
 				auto src = dest ? regmem : reg;
 				auto dst = dest ? reg : regmem;
 
 				const char *fst = reg_table[wide][dst];
 				const char *snd = reg_table[wide][src];
 				fprintf(stdout, "mov %s, %s\n", fst, snd);
-				break;
+			} else {
+				const char *r = reg_table[wide][reg];
+				char eff_addr_calc[16] = { '\0' };
+				const char *eff_addr = regmem_table[regmem];
+
+				if (mode == MemoryMode::SHORT) {
+					uint8_t data = source[ptr++];
+					if (data) {
+						sprintf(eff_addr_calc, "%s + %d", eff_addr, data);
+					} else {
+						sprintf(eff_addr_calc, "%s", eff_addr);
+					}
+				} else if (mode == MemoryMode::WIDE || regmem == 0x6) {
+					uint8_t low = source[ptr++];
+					uint8_t high = source[ptr++];
+					uint16_t data = low | (high << 8);
+
+					if (mode == MemoryMode::WIDE) {
+						if (data) {
+							sprintf(eff_addr_calc, "%s + %d", eff_addr, data);
+						} else {
+							sprintf(eff_addr_calc, "%s", eff_addr);
+						}
+					} else {
+						sprintf(eff_addr_calc, "%d", data);
+					}
+				} else {
+					sprintf(eff_addr_calc, "%s", eff_addr);
+				}
+
+				if (dest) {
+					fprintf(stdout, "mov %s, [%s]\n", r, eff_addr_calc);
+				} else {
+					fprintf(stdout, "mov [%s], %s\n", eff_addr_calc, r);
+				}
 			}
-			case MemoryMode::SHORT:
-			case MemoryMode::WIDE:
-			case MemoryMode::MEMORY:
-			default:
-				break;
-			}
-			ptr += 2;
-			break;
+			
+			continue;
 		}
-		default:
-			ptr += 2;
-			break;
+
+		if (check_opcode(instr, Instruction::IMM_TO_REGMEM)) {
+
+			continue;
+		}
+
+		if (check_opcode(instr, Instruction::MEM_TO_ACC)) {
+
+			continue;
+		}
+
+		if (check_opcode(instr, Instruction::ACC_TO_MEM)) {
+
+			continue;
 		}
 	}
 }
--- homeworks/part1/emulator8086/emulator8086/instructions.h
+++ homeworks/part1/emulator8086/emulator8086/instructions.h
@@ -5,7 +5,11 @@
 namespace emu8086 {
 
 enum Instruction : uint8_t {
-	MOV = 0x22,
+	REGMEM_TOFROM_REG = 0x22,
+	IMM_TO_REGMEM = 0x63,
+	IMM_TO_REG = 0xB,
+	MEM_TO_ACC = 0x50,
+	ACC_TO_MEM = 0x51,
 };
 
 }
